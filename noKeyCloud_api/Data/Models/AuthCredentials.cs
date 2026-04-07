c304a35e0c1d618410637753da951a8febf5b359
MrArond <Jakubr14@gmail.com>
2026-04-07 20:40:40 +0200

    Feat: 6. Add Database tables

---
+++ noKeyCloud_api/Data/Models/AuthCredentials.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace noKeyCloud_api.Data.Models
+{
+    public class AuthCredentials
+    {
+        [Key]
+        public required Guid Id { get; set; }
+        public required string Salt { get; set; }
+        public required string Verifier { get; set; }
+        public required DateTime CreatedAt { get; set; }
+        public DateTime CreationDate { get; set; }
+        public Guid UserId { get; set; }
+        public Users User { get; set; }
+    }
+
+}
---
+++ noKeyCloud_api/Data/Models/Users.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace noKeyCloud_api.Data.Models
+{
+    public class Users
+    {
+        [Key]
+        public required Guid Id { get; set; }
+        public required string Email { get; set; }
+        public required string? Username { get; set; }
+        public required bool? IsActive { get; set; }
+        public required DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public AuthCredentials Credentials { get; set; }
+        public ICollection<UserShares> Shares{ get; set; }
+        public ICollection<PublicLinks> PublicLinks{ get; set; }
+        public ICollection<Sessions> Sessions{ get; set; }
+        public ICollection<UserKeys> UserKeys{ get; set; }
+        public ICollection<RecoveryMethods> RecoveryMethods{ get; set; }
+        public ICollection<SecurityEvents> SecurityEvents{ get; set; }
+        public ICollection<Folders> Folders{ get; set; }
+        public ICollection<Files> Files{ get; set; }
+    }
+}
