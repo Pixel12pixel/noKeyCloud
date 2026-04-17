ce455a720c25f069da080428d1cb2355f8897543
minipaka05 <minpak05@gmail.com>
2026-04-17 17:07:35 +0200

    Test: Add register test

---
+++ tests/noKeyCloud.Application.UnitTests/Features/Users/Register/RegisterUserTests.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using noKeyCloud.Application.Abstractions.Repositories;
+using noKeyCloud.Application.Features.Users.Register;
+
+namespace noKeyCloud_apiUnitTests.Features.Users.Register;
+
+public class RegisterUserTests
+{
+    [Fact]
+    public async Task Should_register_user()
+    {
+        var repoMock = new Mock<IUserRepository>();
+        var handler = new RegisterUserHandler(repoMock.Object);
+        var cmd = new RegisterUserCommand("mine", "minipaka", new byte[] { 75 }, new byte[] { 34 });
+
+        var result = await handler.Handle(cmd,  CancellationToken.None);
+        
+        Assert.True(result.IsSuccess);
+    }
+}
--- tests/noKeyCloud.Application.UnitTests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace noKeyCloud_apiUnitTests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
