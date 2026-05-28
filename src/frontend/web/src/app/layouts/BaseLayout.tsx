4ef1cf8814003deb9a4892b959675196179ce853
xEska1337 <44504000+xEska1337@users.noreply.github.com>
2026-05-28 13:49:06 +0200

    feat: footer

--- src/frontend/web/src/app/layouts/BaseLayout.tsx
+++ src/frontend/web/src/app/layouts/BaseLayout.tsx
@@ -1,15 +1,18 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "@/widgets/header/ui/Header";
 import { Toaster } from "@/shared/ui/sonner";
+import { Footer } from "@/widgets/footer/ui/Footer";
 
 export function BaseLayout() {
     return (
         <div className="relative min-h-screen flex flex-col">
             <Header />
 
-            <main className="flex-1">
+            <main className="flex-1 flex flex-col">
                 <Outlet />
             </main>
+
+            <Footer />
 
             <Toaster richColors position="bottom-right" />
         </div>
